3c0d584f060ae012f39dfdd203f4a00dfa9eaaef
Hidekazu Ueba <hidekazu@example.com>
2026-02-24 13:51:13 +0100

    add PrimaryButton.tsx

--- components/RegisterForm.tsx
+++ components/RegisterForm.tsx
@@ -1,4 +1,3 @@
-
 'use client'
 
 import { register } from '@/app/(auth)/action'
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,32 +1,6 @@
-import { clsx } from "clsx"
-import { cookies } from "next/headers"
+import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export function cn(...inputs: any[]) {
+export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-// export async function auth(){
-//   const cookiesStore = await cookies()
-//   const token = cookiesStore.get('token')
-
-//   let res: Response
-
-//   try{
-//     res = await fetch(`${process.env.API_URL}/api/me`, {
-//       headers : {
-//         Accept: "application/json",
-//         Authorization: `Bearer ${token}`,
-//       }
-//     })
-//   }catch(e){
-//     throw new Error(`Network error while fetching user info: ${String(e)}`);
-//   }
-
-//   if(!res.ok){
-//     return null
-//   }
-
-//   const resJson = await res.json()
-//   return resJson.user
-// }
