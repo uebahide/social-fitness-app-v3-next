b7a9d9955f33b8309919bb53e20153fe383c72d4
Hidekazu Ueba <hidekazu@example.com>
2026-02-25 11:47:43 +0100

    update header ui

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,11 +3,31 @@
 import { useUser } from '@/contexts/UserProvider';
 import { LogoutButton } from './LogoutButton';
 import { useEffect } from 'react';
+import Image from 'next/image';
+import { Button } from './buttons/Button';
+import Link from 'next/link';
 
 export default function Header() {
   const { user, setUser } = useUser();
 
   return (
-    <div className="flex h-10 justify-end border-b border-gray-200">{user && <LogoutButton />}</div>
+    <div className="flex justify-end border-b border-gray-200 px-5 py-3">
+      {user ? (
+        <div className="flex cursor-pointer items-center gap-5">
+          <p>{user.name}</p>
+          {/* todo - replace this icon component with Image component */}
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-amber-100">
+            H
+          </div>
+          <LogoutButton />
+        </div>
+      ) : (
+        <div>
+          <Button color="primary">
+            <Link href="/login">Login</Link>
+          </Button>
+        </div>
+      )}
+    </div>
   );
 }
--- components/LogoutButton.tsx
+++ components/LogoutButton.tsx
@@ -2,17 +2,15 @@
 
 import { logout } from '@/app/(auth)/action';
 import { useActionState } from 'react';
+import { Button } from './buttons/Button';
 
 export const LogoutButton = () => {
   const [state, formAction] = useActionState(logout, null);
   return (
     <form action={formAction}>
-      <button
-        className="bg-brand-secondary-400 hover:bg-brand-secondary-500 m-2 cursor-pointer rounded-lg px-2 text-white shadow-lg"
-        type="submit"
-      >
+      <Button color="secondary" type="submit" className="w-fit rounded-lg text-white">
         logout
-      </button>
+      </Button>
     </form>
   );
 };
