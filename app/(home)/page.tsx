dbc6f34bb43990ae22405b64bd754fa6d7e192a3
uebahide <uebahide@gmail.com>
2026-03-09 15:43:08 +0100

    add placeholder

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -60,14 +60,16 @@ export default async function Home() {
     <div className="grid grid-cols-[3fr_4fr_3fr] grid-rows-2 gap-2">
       <UserProfileCard latestActivity={latestActivity} activityCount={activityCount} />
       <section className="col-span-1 row-span-2 h-screen">
-        <div className="bg-card mt-9 rounded-sm border border-gray-200 p-4">activities</div>
+        <div className="bg-card mt-9 rounded-sm border border-gray-200 p-4">
+          friends' activities are coming soon
+        </div>
       </section>
       <WeatherCard />
       <section className="bg-card col-span-1 row-span-1 rounded-sm border border-gray-200 p-4">
-        championships
+        championships are coming soon
       </section>
       <section className="bg-card col-span-1 row-span-1 rounded-sm border border-gray-200 p-4">
-        events
+        events are coming soon
       </section>
     </div>
   );
