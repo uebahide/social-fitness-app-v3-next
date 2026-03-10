9d60cc22ec3a511cc1b907237c1bc0edbd871d15
uebahide <uebahide@gmail.com>
2026-03-10 16:19:08 +0100

    align text & icon to left with w-full

--- components/LogoutButton.tsx
+++ components/LogoutButton.tsx
@@ -8,10 +8,10 @@ import { LogOutIcon } from 'lucide-react';
 export const LogoutButton = () => {
   const [state, formAction] = useActionState(logout, null);
   return (
-    <form action={formAction}>
+    <form action={formAction} className="w-full">
       <SubmitButton
         color="transparent"
-        className="focus:bg-accent focus:text-accent-foreground data-[variant=destructive]:text-destructive data-[variant=destructive]:focus:bg-destructive/10 data-[variant=destructive]:focus:text-destructive dark:data-[variant=destructive]:focus:bg-destructive/20 [&_svg:not([class*='text-'])]:text-muted-foreground data-[variant=destructive]:*:[svg]:text-destructive! relative flex cursor-default items-center gap-2 rounded-sm p-0 text-sm font-normal text-black outline-hidden select-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 data-[inset]:pl-8 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4"
+        className="focus:bg-accent focus:text-accent-foreground data-[variant=destructive]:text-destructive data-[variant=destructive]:focus:bg-destructive/10 data-[variant=destructive]:focus:text-destructive dark:data-[variant=destructive]:focus:bg-destructive/20 [&_svg:not([class*='text-'])]:text-muted-foreground data-[variant=destructive]:*:[svg]:text-destructive! relative flex w-full cursor-default items-center justify-start gap-2 rounded-sm p-0 text-sm font-normal text-black outline-hidden select-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 data-[inset]:pl-8 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4"
       >
         <LogOutIcon className="h-4 w-4" />
         Log out
